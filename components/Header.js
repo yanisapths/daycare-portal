4df878f607b719c5f92fca3dbbc0801f6033f65e
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2022-12-13 21:59:02 +0700

    add side bar nav

--- components/Header.js
+++ components/Header.js
@@ -1,94 +1,36 @@
 import React, { useState, Fragment } from "react";
 import Image from "next/image";
 import { Menu, Transition } from "@headlessui/react";
-import { SearchIcon, GlobeAltIcon, UsersIcon } from "@heroicons/react/solid";
 import "react-date-range/dist/styles.css"; // main style file
 import "react-date-range/dist/theme/default.css"; // theme css file
-import { DateRangePicker } from "react-date-range";
 import { useRouter } from "next/dist/client/router";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
+import Drawer from "./Drawer";
 
 function Header({ placeholder }) {
   const { data: session } = useSession();
-  const [searchInput, setSearchInput] = useState("");
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const [noOfGuests, setNoOfGuests] = useState(1);
   const router = useRouter();
 
-  const handleSelect = (ranges) => {
-    setStartDate(ranges.selection.startDate);
-    setEndDate(ranges.selection.endDate);
-  };
-
-  const resetInput = () => {
-    setSearchInput("");
-  };
-
-  const search = () => {
-    router.push({
-      pathname: "search",
-      query: {
-        location: searchInput,
-        startDate: startDate.toISOString(),
-        endDate: endDate.toISOString(),
-        noOfGuests,
-      },
-    });
-  };
-
-  const selectionRange = {
-    startDate: startDate,
-    endDate: endDate,
-    key: "selection",
-  };
-
   return (
-    <header className="font-noto sticky top-0 z-50 grid grid-cols-3 shadow-md bg-white p-5 md:px-10 rounded-b-2xl">
+    <header className="font-noto justify-between flex flex-wrap sticky top-0 z-50  shadow-2xl shadow-black/10 p-1 md:px-10 rounded-b-2xl bg-[#FFEAB2]">
       {/* Left */}
-      <div
-        onClick={() => router.push("/")}
-        className="relative flex items-center h-16 cursor-pointer my-auto"
-      >
-        <Image
-          src="/olivelogo.png"
-          alt="/Avatar.png"
-          width={110}
-          height={110}
-          //    layout="fill"
-          objectFit="contain"
-          objectPosition="left"
-        />
+      <div className="flex pt-2">
+        <div>
+          <Drawer />
+        </div>
+        <div
+          onClick={() => router.push("/")}
+          className=" cursor-pointer my-auto"
+        >
+          <h1 className="text-3xl font-bold text-yellow-800">Happy Elders</h1>
+        </div>
       </div>
-
-      {/* Middle  - Search */}
-      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm appearance-none outline-none ">
-        <input
-          value={searchInput}
-          onChange={(e) => setSearchInput(e.target.value)}
-          className=" flex-grow pl-5 text-teal-900 bg-transparent outline-none border-none appearance-none focus:outline-none"
-          type="text"
-          placeholder={placeholder || "ค้นหาศูนย์ดูแล"}
-        />
-        <SearchIcon className="hidden md:inline-flex h-8 bg-teal-500 text-white rounded-full p-2 cursor-pointer md:mx-2" />
-      </div>
-
-      {/* Right */}
-      <div className="flex items-center space-x-4 justify-end text-teal-900">
-        {!session && (
-          <>
-            <Link href="/daycares/">
-              <p className="hidden md:inline text-teal-900 cursor-pointer">
-                เป็นเจ้าของศูนย์ดูแล
-                <GlobeAltIcon className="h-6  md:inline " />
-              </p>
-            </Link>
-          </>
-        )}
+      <div className=" items-end justify-between text-teal-900">
+        {/* Right */}
         <Menu as="div" className="relative inline-block text-left">
-          <div>
-            <Menu.Button className="flex items-center space-x-2 border-2 p-1 rounded-full">
+          <div className="justify-end pt-2 right-0 justify-end">
+            <Menu.Button className="flex border-2 p-1 rounded-full">
               {/* Profile Pic */}
               {!session && (
                 <>
@@ -166,46 +108,6 @@ function Header({ placeholder }) {
           </Transition>
         </Menu>
       </div>
-      {searchInput && (
-        <div className="flex flex-col col-span-3 mx-auto">
-          <DateRangePicker
-            ranges={[selectionRange]}
-            minDate={new Date()}
-            rangeColors={["#7BC6B7"]}
-            onChange={handleSelect}
-          />
-          <div>
-            <div className="flex items-center border-b mb-4">
-              <h2 className="text-2xl flex-grow font-semibold text-teal-900">
-                จำนวนผู้ต้องการดูแล
-              </h2>
-
-              <UsersIcon className="h-5" />
-              <input
-                value={noOfGuests}
-                onChange={(e) => setNoOfGuests(e.target.value)}
-                type="number"
-                min={1}
-                className="w-12 pl-2 text-xl outline-none border-0  text-teal-600 font-semibold border-none appearance-none focus:outline-none "
-              />
-            </div>
-            <div className="flex">
-              <button
-                onClick={resetInput}
-                className="flex-grow text-gray-500 text-2xl font-semibold"
-              >
-                ยกเลิก
-              </button>
-              <button
-                onClick={search}
-                className="flex-grow text-teal-600 text-2xl font-semibold"
-              >
-                ค้นหา
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
     </header>
   );
 }
