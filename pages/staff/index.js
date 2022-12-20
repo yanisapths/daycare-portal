3cb88e562fe6dd5ff41a090c273c247b50f68a28
iionayne25 <alinqweqwz@gmail.com>
2022-12-20 19:45:41 +0700

    delete layout

--- pages/staff/index.js
+++ pages/staff/index.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import Layout from '../../components/Layout'
-import Head from 'next/head'
+import React from "react";
+
+import Head from "next/head";
 const index = () => {
   return (
     <div>
@@ -8,10 +8,8 @@ const index = () => {
         <title>Daycare | Staff </title>
         <link rel="icon" href="favicon.ico" />
       </Head>
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
