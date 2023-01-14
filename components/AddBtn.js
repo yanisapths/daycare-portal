e0bf931de14f2534e4bdc0d9c79daaefc0db7938
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-01-14 09:55:55 +0700

    remove space

--- components/AddBtn.js
+++ components/AddBtn.js
@@ -5,7 +5,7 @@ const AddBtn = () => {
   return (
     <>
       <button
-        className="  border-2  bg-[#F3BD33]/80  hover:bg-[#6C5137]/70 text-[#6C5137]
+        className="border-2 bg-[#F3BD33]/80 hover:bg-[#6C5137]/70 text-[#6C5137]
                 font-bold text-base sm:text-sm hover:text-white w-20  hove:border-transparent rounded-2xl
                 sm:h-8 lg:h-10 lg:w-24"
       >
