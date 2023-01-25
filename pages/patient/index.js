e5ec5c21db298fdbed74a3a48bdaa897cb2ccc51
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-01-25 10:16:38 +0700

    add: patient to side bar

--- pages/patient/index.js
+++ pages/patient/index.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Header from "../../components/Header";
+import FooterSocial from "../../components/FooterSocial";
+import Head from "next/head";
 
 function Patient() {
   return (
-    <div>index</div>
-  )
+    <div>
+      <Head>
+        <title>Clinic | Patient Report </title>
+        <link rel="icon" href="favicon.ico" />
+      </Head>
+
+      <div className="divide-y divide-[#A17851] divide-opacity-30">
+        <Header />
+
+        <main className="main">
+          <div className="pageTitle">แบบบันทึกรายงานผู้ป่วย</div>
+          <div></div>
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default Patient
+export default Patient;
