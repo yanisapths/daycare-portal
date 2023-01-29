5daa300f0c23249092bef38d3dabe03ce4365a74
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-01-29 15:10:02 +0700

    style(home): update color on cards

--- components/common/BannerCard.js
+++ components/common/BannerCard.js
@@ -2,14 +2,14 @@ import React from "react";
 
 function BannerCard({ username }) {
   return (
-    <div className="rounded-2xl shadow-lg shadow-[#ECE656]/60">
-      <div className="md:flex rounded-2xl bg-[#F5F2AA] p-2 justify-between">
+    <div className="rounded-2xl shadow-lg shadow-[#A17851]/60 text-[#FFFBF2]">
+      <div className="md:flex rounded-2xl bg-[#A17851] p-2 justify-between pb-4">
         <div className="px-6 py-4">
-          <div className="flex">
-            <p className="h4 pr-2 text-black/80">Welcome, </p>
+          <div className="flex text-[#FFFBF2]">
+            <p className="h4 pr-2 text-[#FFFBF2]">Welcome, </p>
             <p className="h4">{username}</p>
           </div>
-          <p className="h6 text-black/ mt-2 ">Administrator</p>
+          <p className="h6 mt-2 ">Administrator</p>
         </div>
         <div className="caption px-6 md:h5 md:px-8 md:mt-2">
           {new Date().toLocaleDateString("th-TH", {
