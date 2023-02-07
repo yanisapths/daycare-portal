53e55722a66c9efb9f90f3d2e74fb7c763e439d0
iionayne25 <alinqweqwz@gmail.com>
2023-02-07 23:31:53 +0700

    style:font size, position type

--- pages/course/course_view/detailView.js
+++ pages/course/course_view/detailView.js
@@ -28,7 +28,7 @@ function detailView({
         open={open}
         onClose={handleClose}
         maxWidth="xl"
-        sx={{ "& .MuiDialog-paper": { width: "60%", boxShadow:'none' } }}
+        sx={{ "& .MuiDialog-paper": { width: "70%", boxShadow:'none' } }}
         BackdropProps={{style:{ opacity: 0.2, backgroundColor: "rgba(0, 0, 0,0.35)",transition:'none' }}}
       >
         <DialogTitle
@@ -41,17 +41,17 @@ function detailView({
             fontWeight: "bold",
           }}
         >
-          <div className="flex flex-row ">
-            <div className="basis-1/4 flex self-start xl:basis-2/12">{name}</div>
+          <div className="flex flex-row sm:grid sm:grid-col-2 ">
+            <div className="basis-1/4 flex self-start  xl:basis-2/12 xxl:text-3xl md:text-xl md:basis-1/4 sm:text-lg sm:basis-2/5">{name}</div>
           
-          <div className="flex basis-2/6 
-          justify-start items-center text-center w-fit xl:pl-2">
-            <div className="rounded-full bg-[#A5A6F6]/20 text-[#7879F1] text-xs px-2 py-1 ">
+          <div className="flex basis-2/6 justify-start items-center text-center w-fit xl:pl-4
+                          sm:col-start-2 ">
+            <div className="rounded-full bg-[#A5A6F6]/20 text-[#7879F1] text-xs px-2 py-1 xxl:px-4">
                {type}
                </div>
            
           </div>
-          <div className="flex basis-5/12 xl:6/12 xl:ml-10 gap-2 justify-end  text-gray-400 cursor-pointer ">
+          <div className="flex basis-5/12 xl:6/12 xl:ml-10 gap-2 justify-end  text-gray-400 cursor-pointer sm:col-start-2 sm:pt-2">
             <EditIcon />
             <DeleteIcon />
           </div>
@@ -63,21 +63,21 @@ function detailView({
           <Box>
             {/* type and price */}
             <div className="flex flex-col text-center rounded-xl mx-5 ">
-              <div className="divide-y-4 divide-dashed">
-              <div className="justify-between flex flex-row text-black/70">
+              <div className="divide-y-2 divide-dashed">
+              <div className="justify-between flex flex-row text-black/70 sm:text-xs">
                 <p>การรักษา</p>
                 <p>ราคา(บาท)</p>
               </div>
               <div className="flex flex-col pt-3 space-y-4 text-black/70">
-                <div className="flex flex-row justify-between bg-[#ffdf8e]/50 rounded-3xl mx-2 py-2 px-3">
+                <div className="flex flex-row justify-between bg-[#ffdf8e]/50 rounded-3xl mx-2 py-2 px-3 sm:px-2 sm:mx-0 sm:text-xs">
                     <p>Ultrasound</p>
                     <p>1500</p>
                 </div>
-                <div className="flex flex-row justify-between bg-[#ffdf8e]/50 rounded-3xl mx-2 py-2 px-3">
+                <div className="flex flex-row justify-between bg-[#ffdf8e]/50 rounded-3xl mx-2 py-2 px-3 sm:px-2 sm:mx-0 sm:text-xs">
                     <p>Laser</p>
                     <p>990</p>
                 </div>
-                <div className="flex flex-row justify-between bg-[#ffdf8e]/50 rounded-3xl mx-2 py-2 px-3">
+                <div className="flex flex-row justify-between bg-[#ffdf8e]/50 rounded-3xl mx-2 py-2 px-3 sm:px-2 sm:mx-0 sm:text-xs">
                     <p>จัดกระดูก</p>
                     <p>690</p>
                 </div>
@@ -85,12 +85,16 @@ function detailView({
               </div>
               
               {/*total time and total price */}
-              <div className="flex flex-col divide-y-4 divide-dashed mt-8 text-black/70 ">
-                <div className="flex justify-start">ค่าใช้จ่ายและระยะเวลาการรักษา</div>
-                <div className="flex flex-col justify-start items-start pt-3 text-lg ">
+              <div className="flex flex-col divide-y-2 divide-dashed mt-8 sm:mt-6 text-black/70 sm:text-xs">
+                <div className="flex justify-start sm:justify-center ">
+                <p className="sm:hidden">ค่าใช้จ่ายและระยะเวลาการรักษา</p>
+                <p className="text-[15px] md:hidden lg:hidden xl:hidden xxl:hidden xxxl:hidden ">ค่าใช้จ่ายและระยะเวลา</p>
+                </div>
+                
+                <div className="flex flex-col justify-start items-start pt-3 text-lg sm:text-sm sm:items-center">
                   <div>คอร์ส 3 ครั้ง</div>
                   <div>2 ชั่วโมง / ครั้ง</div>
-                  <div className="flex self-end text-xl font-semibold">
+                  <div className="flex self-end text-xl font-semibold sm:text-lg sm:self-center sm:pt-5">
                   รวม 15990 บาท
                 </div>
                 </div>
