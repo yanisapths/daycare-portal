b1d09278fa1f444677eeff0d81e9e6ca48e7c511
yanisapths <yanisa_poongthaisong@cmu.ac.th>
2023-03-06 08:52:09 +0700

    fix: card signin

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -15,7 +15,7 @@ function signIn({ providers }) {
       {/* main */}
       <div className="mx-auto  h-screen bg-cover overflow-hidden flex justify-center items-center align-middle bg-[#A17851]  max-w-screen-md">
         <div
-          className="flex flex-col justify-center text-center xxl:w-1/3 xxl:h-5/6 xl:w-2/5 xl:h-3/4 lg:w-2/5 lg:h-3/4 md:w-3/5 md:h-3/4 sm:w-5/6 sm:h-3/4
+          className="flex flex-col justify-center text-center xxl:w-1/3 xxl:h-5/6 xl:w-2/5 xl:h-5/6 lg:w-2/5 lg:h-5/6 md:w-3/5 md:h-3/4 sm:w-5/6 sm:h-5/6
         bg-white backdrop-blur-sm drop-shadow-lg rounded-xl align-middle"
         >
           <div className="">
@@ -33,7 +33,7 @@ function signIn({ providers }) {
               <p className="block ">Olive Physiotherapy Clinic Management</p>
             </p>
           </div>
-          <div className="mt-8 mb-0 grid grid-rows-3 gap-5 content-center mx-auto">
+          <div className="mt-4 mb-0 grid grid-rows-3 gap-5 content-center mx-auto">
             <div
               className="grid grid-col-2 content-center cursor-pointer rounded-full bg-gradient-to-r from-orange-50 via-red-100 to-pink-100
                hover:text-red-600 active:text-opacity-75 focus:outline-none focus:ring shadow-gray-200 shadow-xl "
@@ -82,28 +82,6 @@ function signIn({ providers }) {
                 />
               </div>
             </div>
-            {/* <div className="flex justify-center">
-              <div className="flex cursor-pointer rounded-full bg-gradient-to-r from-teal-50 via-blue-100 to-sky-100 hover:text-blue-600 active:text-opacity-75 focus:outline-none focus:ring shadow-gray-200 shadow-xl">
-                <BtnLogin provider={providers.facebook} />
-                <Image
-                  src="/facebook.png"
-                  alt="/Avatar.png"
-                  width={55}
-                  height={10}
-                />
-              </div>
-            </div> */}
-            {/* <div className="flex justify-center">
-              <div className="flex cursor-pointer rounded-full bg-gradient-to-r from-green-50 via-green-100 to-teal-100 hover:text-emerald-600 active:text-opacity-75 focus:outline-none focus:ring shadow-gray-200 shadow-xl">
-                <BtnLogin provider={providers.line} />
-                <Image
-                  src="/line.png"
-                  alt="/Avatar.png"
-                  width={55}
-                  height={10}
-                />
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
