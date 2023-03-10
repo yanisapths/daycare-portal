79dc6a17fed0c343351a41eff7f04c6d63e7a860
iionayne25 <alinqweqwz@gmail.com>
2023-03-10 22:43:24 +0700

    style: hidden email change to another text

--- components/FooterSocial.js
+++ components/FooterSocial.js
@@ -13,13 +13,13 @@ function FooterSocial() {
   };
   return (
     <footer className="bg-[#FFFBF2]  bottom-0 text-[#372B20] py-2 h-auto">
-      <div className="max-w-screen px-4 pb-2 mx-auto sm:px-6 lg:px-14">
-        <div className="sm:items-center sm:justify-between sm:flex">
+      <div className="max-w-screen px-4 pb-2 mx-auto sm:px-2 lg:px-14">
+        <div className="sm:items-center sm:justify-between sm:flex ">
           <nav className="flex justify-between items-center text-center space-x-4 text-sm font-medium sm:justify-start">
             <div>
-              <p className="text-base pt-2 text-center font-bold">269492 Project</p>
-              <p className="text-base">Faculty of Engineering</p>
-              <p className="text-base">Chiangmai University</p>
+              <p className="text-base pt-2 text-center font-bold sm:text-sm">269492 Project</p>
+              <p className="text-base sm:text-xs">Faculty of Engineering</p>
+              <p className="text-base sm:text-xs">Chiangmai University</p>
             </div>
             <div className="text-center">
               <Image
@@ -28,13 +28,14 @@ function FooterSocial() {
                 height="90"
                 layout="fixed"
               />
-              <p className=" font-bold italic text-xl">Olive</p>
-              <p className="text-lg italic">Made by Happy Elders</p>
+              <p className=" font-bold italic text-xl sm:text-sm">Olive</p>
+              <p className="text-lg italic sm:text-xs">Made by Happy Elders</p>
             </div>
-            <div>
-              <p className="font-semibold text-center text-base">Contact Us</p>
+            <div className="">
+              <p className="font-semibold text-center text-base sm:text-sm">Contact Us</p>
               <Mailto email="olive.happyelders@gmail.com" subject="Hello Yanisa, Pavinee!" body="">
-                   <h4 className="text-base">📧 olive.happyelders@gmail.com</h4>
+                   <p className="text-base sm:hidden "> olive.happyelders@gmail.com</p>
+                   <p className="md:hidden lg:hidden xl:hidden xxl:hidden text-base cursor-pointer sm:text-xs sm:pt-2 text-gray-500 ">Click  to send email</p>
               </Mailto>
               
             </div>
